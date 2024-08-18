8dfe921d5e0d9a4b2bc96939e2cd390cdcb88754
Priyam Pradhan <priyampradhan07@gmail.com>
2024-08-19 02:02:25 +0530

    added sessionID management

--- src/main/java/com/priyam/security/HelloController.java
+++ src/main/java/com/priyam/security/HelloController.java
@@ -1,5 +1,6 @@
 package com.priyam.security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("Hello")            //spring security automatically adds login and also logout feature
-    public String greet(){              //username: user :: password: generated during application run (for dev purpose only)
-        return "Hello World";
+    public String greet(HttpServletRequest request){              //username: user :: password: generated during application run (for dev purpose only)
+        return "Hello World" + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){            //manage sessionID
+        return "Priyam " + request.getSession().getId();
     }
 }
