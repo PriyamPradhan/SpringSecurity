68c7809e49023267793c7a89d619b456bed34c2d
Priyam Pradhan <priyampradhan07@gmail.com>
2024-08-26 00:52:52 +0530

    added home page to avoid home page error

--- src/main/java/com/priyam/security/HelloController.java
+++ src/main/java/com/priyam/security/HelloController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("")                                       //added Home page api
+    public String home(HttpServletRequest request){
+        return "Basic Home Page " + request.getSession().getId();
+    }
+
     @GetMapping("Hello")            //spring security automatically adds login and also logout feature
     public String greet(HttpServletRequest request){              //username: user :: password: generated during application run (for dev purpose only)
-        return "Hello World" + request.getSession().getId();
+        return "Hello World " + request.getSession().getAttribute(String.valueOf(request));
     }
 
     @GetMapping("about")
     public String about(HttpServletRequest request){            //manage sessionID
         return "Priyam " + request.getSession().getId();
     }
+
 }
